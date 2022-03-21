9454c1d36304180c85588552111add79a44c33c5
aquila <aquila.mason.2014@gmail.com>
2022-03-21 18:18:54 +0800

    Add the parameter destination_prefix_list_id for vpc route.

--- variables.tf
+++ variables.tf
@@ -107,6 +107,7 @@ variable "vpc_attachments" {
       route_table_id              = string
       destination_cidr_block      = string
       destination_ipv6_cidr_block = string
+      destination_prefix_list_id  = string
     }))
     transit_gateway_default_route_table_association = bool
     transit_gateway_default_route_table_propagation = bool
